ec7144d8cf8c1cf706ab3c62e043b4e1d7c2c844
FarheenShakeel <90086212+FarheenShakeel@users.noreply.github.com>
2022-12-18 21:36:30 +0530

    Add files via upload

---
+++ Day17_33_StringFuncStrlen.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char str[50];
+    printf("Enter any string: ");
+    scanf("%s",str);
+    printf("Length of the string is: %d",strlen(str));
+
+    return 0;
+}
